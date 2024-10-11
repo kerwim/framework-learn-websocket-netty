17293b40d802cd4492991ab15de5b34ac0bd9ef0
mac <0924>
2024-10-11 23:52:48 +0800

    intro

---
+++ src/test/java/com/kerwim/frameworklearnwebsocketnetty/FrameworkLearnWebsocketNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnwebsocketnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkLearnWebsocketNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
